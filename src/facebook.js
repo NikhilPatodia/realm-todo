9a56179f9a7db46f632ae69460f12af130427096
NikhilPatodia <npatodia085@gmail.com>
2024-06-24 10:41:52 +0530

    add showButtons

---
+++ src/facebook.js
@@ -0,0 +1,13 @@
+export default {
+    install(Vue) {
+      Vue.mixin({
+        mounted() {
+          if (window.FB) {
+            window.FB.XFBML.parse();
+            console.log("run facebook")
+          }
+        }
+      });
+    }
+  };
+  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-
+import FacebookPlugin from './facebook.js';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import * as Realm from "realm-web";
@@ -10,6 +10,7 @@ new Realm.App({ id: "application-1-yyltwyf" });
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(FacebookPlugin);
 app.use(router)
 // app.use(vue3GoogleLogin, {
 //     clientId: '104231573976-2gras7klqs117s3qvr3tm2k3q8h69h1i.apps.googleusercontent.com'
