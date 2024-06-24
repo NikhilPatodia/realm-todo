e0bb7c972cd4403a36e0d5aba8e23b377d2a589d
NikhilPatodia <npatodia085@gmail.com>
2024-06-24 10:46:40 +0530

    add showButtons

--- src/main.js
+++ src/main.js
@@ -11,7 +11,20 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(FacebookPlugin);
+
 app.use(router)
+router.afterEach((to, from) => {
+    if (window.FB) {
+      window.FB.XFBML.parse();
+    }
+    if (window.google && window.google.accounts) {
+      window.google.accounts.id.initialize({
+        client_id: '104231573976-2gras7klqs117s3qvr3tm2k3q8h69h1i.apps.googleusercontent.com',
+        callback: (response) => console.log(response)
+      });
+      window.google.accounts.id.prompt();
+    }
+  });
 // app.use(vue3GoogleLogin, {
 //     clientId: '104231573976-2gras7klqs117s3qvr3tm2k3q8h69h1i.apps.googleusercontent.com'
 //   })
