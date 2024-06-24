6ab2a93bcde16a14083d9c14428db69f580fe0e0
NikhilPatodia <npatodia085@gmail.com>
2024-06-24 12:02:16 +0530

    add showButtons

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ app.use(router)
 initializeGoogleOneTap();
 
 // Function to initialize Google One Tap
-function initializeGoogleOneTap() {
+export function initializeGoogleOneTap() {
   if (window.google && window.google.accounts) {
     window.google.accounts.id.initialize({
       client_id: '104231573976-2gras7klqs117s3qvr3tm2k3q8h69h1i.apps.googleusercontent.com', // Replace with your actual Google client ID
